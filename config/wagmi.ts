7c72800ee21170a74e0c9dfe9b44a7892a7e03b5
osmdmr993 <129073613+osmdmr993@users.noreply.github.com>
2026-08-19 13:16:18 +0200

    config/wagmi.ts

--- config/wagmi.ts
+++
@@ -1,9 +0,0 @@
-import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { arbitrum, base, polygon, mainnet } from 'wagmi/chains';
-
-export const config = getDefaultConfig({
-  appName: 'DiceDuel P2P',
-  projectId: '04353018244199c011e496a70e883838', // Standart demo project id
-  chains: [base, arbitrum, polygon, mainnet],
-  ssr: true,
-});
